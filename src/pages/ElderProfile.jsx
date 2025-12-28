07c5e4ae2d879a41b9f56ddde5fc625439f6a4de
Youssef5351 <155520872+Youssef5351@users.noreply.github.com>
2025-12-29 00:05:41 +0200

    Update ElderProfile.jsx

--- src/pages/ElderProfile.jsx
+++ src/pages/ElderProfile.jsx
@@ -13,7 +13,7 @@ const ElderProfile = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-          const res = await axios.get(`http://localhost:5000/getPatient/${id}`);
+          const res = await axios.get(`https://young-innovator-backend.vercel.app/getPatient/${id}`);
           console.log(res.data.patient)
           setPatientDetail(res.data.patient)
       }
